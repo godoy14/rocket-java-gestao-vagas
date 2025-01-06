e09b130945f2cda2acaae2dc0afe41ac6baf1220
godoy14 <joao.sgodoy@hotmail.com>
2025-01-06 19:32:35 -0300

    Adicionando banco de dados em memória para testes e teste de integração para createJob

--- src/main/java/com/godoy/rocket_gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/com/godoy/rocket_gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -2,9 +2,15 @@ package com.godoy.rocket_gestao_vagas.modules.company.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateJobDTO {
 
     @Schema(example = "Vaga para programador", requiredMode = RequiredMode.REQUIRED)
